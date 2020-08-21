2a6d51ebf70de2389f64c9169721c74d298db8df
Vaibhav Jindal <vaibhav.jindal.2001@gmail.com>
2020-08-22 00:49:32 +0530

    add DishDetail component, update Main and Menu Component

--- components/MainComponent.js
+++ components/MainComponent.js
@@ -1,5 +1,7 @@
 import React, { Component } from "react";
+import { View } from "react-native";
 import Menu from "./MenuComponent";
+import DishDetail from "./DishDetailComponent";
 import { DISHES } from "../shared/dishes";
 
 class Main extends Component {
@@ -7,10 +9,30 @@ class Main extends Component {
     super(props);
     this.state = {
       dishes: DISHES,
+      selectedDish: null,
     };
   }
+
+  onDishSelect(dishId) {
+    this.setState({ selectedDish: dishId });
+  }
+
   render() {
-    return <Menu dishes={this.state.dishes} />;
+    return (
+      <View>
+        <Menu
+          dishes={this.state.dishes}
+          onPress={(dishId) => this.onDishSelect(dishId)}
+        />
+        <DishDetail
+          dish={
+            this.state.dishes.filter(
+              (dish) => dish.id === this.state.selectedDish
+            )[0]
+          }
+        />
+      </View>
+    );
   }
 }
 
