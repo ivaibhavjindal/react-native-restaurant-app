0f2cecf79e21266fa7f63d049e0200d57ef18cf0
Vaibhav Jindal <vaibhav.jindal.2001@gmail.com>
2020-08-23 15:09:23 +0530

    update Home component

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import React from "react";
-import Main from "./components/MainComponent";
 import "react-native-gesture-handler";
 import { NavigationContainer } from "@react-navigation/native";
+import Main from "./components/MainComponent";
 
 export default function App() {
   return (
