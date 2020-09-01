20581f565a9a2817f96a9aa1a6e5f41ac5774eca
Vaibhav Jindal <vaibhav.jindal.2001@gmail.com>
2020-09-01 16:19:18 +0530

    add animations to home component

--- components/HomeComponent.js
+++ components/HomeComponent.js
@@ -1,6 +1,6 @@
-import React from "react";
+import React, { Component } from "react";
 import { connect } from "react-redux";
-import { View, ScrollView, Text } from "react-native";
+import { View, Text, Animated, Easing } from "react-native";
 import { Card } from "react-native-elements";
 import { baseUrl } from "../shared/baseUrl";
 import Loading from "./LoadingComponent";
@@ -39,28 +39,75 @@ const RenderItem = ({ item, isLoading, errMess }) => {
   }
 };
 
-function Home({ dishes, promotions, leaders }) {
-  return (
-    <ScrollView>
-      <RenderItem
-        isLoading={dishes.isLoading}
-        errMess={dishes.errMess}
-        item={dishes.dishes.filter((dish) => dish.featured)[0]}
-      />
-      <RenderItem
-        isLoading={promotions.isLoading}
-        errMess={promotions.errMess}
-        item={
-          promotions.promotions.filter((promotion) => promotion.featured)[0]
-        }
-      />
-      <RenderItem
-        isLoading={leaders.isLoading}
-        errMess={leaders.errMess}
-        item={leaders.leaders.filter((leader) => leader.featured)[0]}
-      />
-    </ScrollView>
-  );
+class Home extends Component {
+  constructor(props) {
+    super(props);
+    this.animatedValue = new Animated.Value(0);
+    this.animate = this.animate.bind(this);
+  }
+
+  componentDidMount() {
+    this.animate();
+  }
+
+  animate() {
+    this.animatedValue.setValue(0);
+    Animated.timing(this.animatedValue, {
+      toValue: 8,
+      duration: 8000,
+      easing: Easing.linear,
+      useNativeDriver: true,
+    }).start(this.animate);
+  }
+
+  render() {
+    const { dishes, promotions, leaders } = this.props;
+    const xpos1 = this.animatedValue.interpolate({
+      inputRange: [0, 1, 3, 5, 8],
+      outputRange: [1200, 600, 0, -600, -1200],
+    });
+    const xpos2 = this.animatedValue.interpolate({
+      inputRange: [0, 2, 4, 6, 8],
+      outputRange: [1200, 600, 0, -600, -1200],
+    });
+    const xpos3 = this.animatedValue.interpolate({
+      inputRange: [0, 3, 5, 7, 8],
+      outputRange: [1200, 600, 0, -600, -1200],
+    });
+    return (
+      <View style={{ flex: 1, flexDirection: "row", justifyContent: "center" }}>
+        <Animated.View
+          style={{ width: "100%", transform: [{ translateX: xpos1 }] }}
+        >
+          <RenderItem
+            isLoading={dishes.isLoading}
+            errMess={dishes.errMess}
+            item={dishes.dishes.filter((dish) => dish.featured)[0]}
+          />
+        </Animated.View>
+        <Animated.View
+          style={{ width: "100%", transform: [{ translateX: xpos2 }] }}
+        >
+          <RenderItem
+            isLoading={promotions.isLoading}
+            errMess={promotions.errMess}
+            item={
+              promotions.promotions.filter((promotion) => promotion.featured)[0]
+            }
+          />
+        </Animated.View>
+        <Animated.View
+          style={{ width: "100%", transform: [{ translateX: xpos3 }] }}
+        >
+          <RenderItem
+            isLoading={leaders.isLoading}
+            errMess={leaders.errMess}
+            item={leaders.leaders.filter((leader) => leader.featured)[0]}
+          />
+        </Animated.View>
+      </View>
+    );
+  }
 }
 
 export default connect(mapStateToProps)(Home);
